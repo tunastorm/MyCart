279699ad31229fe96d2f4cd6a7a8dbcb04216dfe
tunastorm <ycw921@naver.com>
2024-06-16 14:19:32 +0900

    Update: 프로필 이미지 선택화면 기능구현 완료

---
+++ MyCart/View/Auth/SelectPhotoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SelectPhotoCollectionViewCell.swift
+//  MyCart
+//
+//  Created by 유철원 on 6/16/24.
+//
+
+import UIKit
+
+class SelectPhotoCollectionViewCell: UICollectionViewCell {
+    
+}
--- MyCart/View/Auth/SelectPhotoCollectionViewController.swift
+++ MyCart/View/Auth/SelectPhotoCollectionViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class SelectPhotoViewController: UIViewController {
 
+    var delegate: SignUpViewController?
+    
+    var photoList: [UIImage]?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    func setPhotoList() {
+        for i in 0...11 {
+            photoList?.append(UIImage(named: ""))
+        }
+    }
     
-
+    
+    
 }
